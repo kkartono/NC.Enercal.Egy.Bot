ccbd540a82d71542c4ee2c5e787b65566cf7732b
Karen <k.kartono-t@ncit.nc>
2018-06-20 08:57:20 +1100

    initiale commit

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.QnABot.sln -p:DeployOnBuild=true -p:PublishProfile=ncenercalegybot-Web-Deploy.pubxml -p:Password=apip55Yznwmz3sXdTRLsyYNJHosfwAGnPrqMw697l2WkfqlWdhm9stnKbTbz
+
